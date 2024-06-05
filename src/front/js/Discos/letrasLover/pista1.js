cd44b552f0694236ff3e727d448601a42136e83c
Diana Jimenez Roo <149723555+DianaJRoo@users.noreply.github.com>
2024-06-05 23:59:23 +0000

    letravista listo

--- src/front/js/Discos/letrasLover/pista1.js
+++ src/front/js/Discos/letrasLover/pista1.js
@@ -12,67 +12,69 @@ export const Pista1 = () => {
 
     return <>
 
-        <div id='hero-body' className="flex flex-col sm:flex-row mx-auto px-4 md:px-8 lg:px-16 xl:px-20 py-20 min-h-screen mt-0 mb-5 " style={{ backgroundImage: 'url("https://i.pinimg.com/736x/d2/10/34/d21034e458359dd4ad8fdf312613b597.jpg")', backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh' }}>
+        <div id='hero-body' className="flex flex-col sm:flex-row mx-auto px-4 md:px-8 lg:px-16 xl:px-20 py-20 min-h-screen mt-0 mb-0 " style={{ backgroundImage: 'url("https://i.pinimg.com/736x/d2/10/34/d21034e458359dd4ad8fdf312613b597.jpg")', backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh' }}>
 
-            <div id='hero' className='d-flex  justify-center'  >
-                <div className="max-w-sm rounded-lg overflow-hidden shadow-lg flex justify-around hover:scale-110 transition-transform duration-300 ease-in-out bg-white" >
+            <div id='hero' className='d-flex  justify-center'style={{ width: '55rem', }}  >
+                <div className="  card w-full max-w-xl rounded-lg  overflow-y-scroll h-full shadow-lg flex hover:scale-110 transition-transform duration-300 ease-in-out bg-slate-100" >
 
                     <div className="px-6 py-4 ">
-                        <div className="font-bold text-l mb-2">
-                            How many days did I spend
-                            Thinkin' 'bout how you did me wrong, wrong, wrong?
-                            Lived in the shade you were throwin'
-                            'Til all of my sunshine was gone, gone, gone
-                            And I couldn't get away from you
-                            In my feelings more than Drake, so yeah
-                            Your name on my lips, tongue tied
-                            Free rent, livin' in my mind
-                            But then something happened one magical night
+                        <div className="font-bold text-sm leading-relaxed ">
+                            
+                            How many days did I spend <br/>
+                            Thinkin' 'bout how you did me wrong, wrong, wrong?<br/>
+                            Lived in the shade you were throwin'<br/>
+                            'Til all of my sunshine was gone, gone, gone<br/>
+                            And I couldn't get away from you<br/>
+                            In my feelings more than Drake, so yeah<br/>
+                            Your name on my lips, tongue tied<br/>
+                            Free rent, livin' in my mind<br/>
+                            But then something happened one magical night<br/><br/>
+                            
+                            
+                            I forgot that you existed<br/>
+                            And I thought that it would kill me, but it didn't<br/>
+                            And it was so nice<br/>
+                            So peaceful and quiet<br/><br/>
+                           
+                            I forgot that you existed<br/>
+                            It isn't love, it isn't hate<br/>
+                            It's just indifference<br/><br/>
 
-                            I forgot that you existed
-                            And I thought that it would kill me, but it didn't
-                            And it was so nice
-                            So peaceful and quiet
+                            I forgot that you<br/>
+                            Got out some popcorn<br/>
+                            As soon as my rep starting going down, down, down<br/>
+                            Laughed on the schoolyard<br/>
+                            As soon as I tripped up and hit the ground, ground, ground<br/>
+                            And I would've stuck around for ya<br/>
+                            Would've fought the whole town, so yeah<br/>
+                            Would've been right there front row<br/>
+                            Even if nobody came to your show<br/>
+                            But you showed who you are, then one magical night<br/><br/>
 
-                            I forgot that you existed
-                            It isn't love, it isn't hate
-                            It's just indifference
+                            I forgot that you existed<br/>
+                            And I thought that it would kill me, but it didn't<br/>
+                            And it was so nice<br/>
+                            So peaceful and quiet<br/><br/>
 
-                            I forgot that you
-                            Got out some popcorn
-                            As soon as my rep starting going down, down, down
-                            Laughed on the schoolyard
-                            As soon as I tripped up and hit the ground, ground, ground
-                            And I would've stuck around for ya
-                            Would've fought the whole town, so yeah
-                            Would've been right there front row
-                            Even if nobody came to your show
-                            But you showed who you are, then one magical night
+                            I forgot that you existed<br/>
+                            It isn't love, it isn't hate<br/>
+                            It's just indifference<br/><br/>
 
-                            I forgot that you existed
-                            And I thought that it would kill me, but it didn't
-                            And it was so nice
-                            So peaceful and quiet
+                            I forgot that you<br/>
+                            Sent me a clear message<br/>
+                            Taught me some hard lessons<br/>
+                            I just forget what they were<br/>
+                            It's all just a blur<br/><br/>
 
-                            I forgot that you existed
-                            It isn't love, it isn't hate
-                            It's just indifference
+                            I forgot that you existed<br/>
+                            And I thought that it would kill me, but it didn't<br/>
+                            And it was so nice<br/>
+                            So peaceful and quiet<br/><br/>
 
-                            I forgot that you
-                            Sent me a clear message
-                            Taught me some hard lessons
-                            I just forget what they were
-                            It's all just a blur
-
-                            I forgot that you existed
-                            And I thought that it would kill me, but it didn't
-                            And it was so nice
-                            So peaceful and quiet
-
-                            I forgot that you existed
-                            I did, I did, I did
-                            It isn't hate, it's just indifference
-                            It isn't love, it isn't hate
+                            I forgot that you existed<br/>
+                            I did, I did, I did<br/>
+                            It isn't hate, it's just indifference<br/>
+                            It isn't love, it isn't hate<br/>
 
                         </div>
 
@@ -82,7 +84,10 @@ export const Pista1 = () => {
 
             </div>
 
+         <div className=" flex justify-center items-center">
+         <iframe width="560" height="315" src="https://www.youtube.com/embed/p1cEvNn88jM?si=G9nNZoTTfnmlq4uB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
+         </div>
 
 
 
@@ -91,10 +96,11 @@ export const Pista1 = () => {
         </div>
 
         <div className='border-y-4 flex justify-center items-center py-4 gap-6 shadow'>
-            <div onClick={() => actions.logOut()} className='flex items-center gap-2 cursor-pointer'>
-                <img src='https://numero.twic.pics/2024-03/taylor-swift-album-numero-magazine1.jpg?twic=v1/quality=83/truecolor=true/output=jpeg' className='flag h-10 w-10 bg-orange-300 rounded-full top-1 start-1' />
-                <p className="font-bold text-xl">Salir</p>
+        <div className='flex items-center gap-2 cursor-pointer'>
+                <img src='https://thenoiseperu.com/wp-content/uploads/2022/01/ddiulc3-4e00352f-2724-47da-94cb-8cf91e7f2982.jpg' className='flag h-20 w-20 bg-orange-300 rounded-full top-1 start-1' />
+               <Link to="/lover"><p className="font-bold text-xl">Atrás</p></Link> 
             </div>
+
 
         </div >
 
