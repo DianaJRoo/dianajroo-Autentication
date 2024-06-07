bad21de270ce4fae7228fd93fd48003641031a92
Diana Jimenez Roo <149723555+DianaJRoo@users.noreply.github.com>
2024-06-07 05:37:56 +0000

    cambios en el navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Context } from '../store/appContext';
 import { useContext } from 'react';
@@ -6,9 +6,12 @@ import { useContext } from 'react';
 
 export const Navbar = () => {
   const { actions } = useContext(Context);
+ const [showProfileMenu, setShowProfileMenu] = useState(false);
 
+  const toggleProfileMenu = () => {
+    setShowProfileMenu(!showProfileMenu);
 
-
+  };
 	return (
 		<nav className="bg-pink-800">
   <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -48,20 +51,21 @@ export const Navbar = () => {
       
         <div className="relative ml-3">
           <div>
-            <button type="button" className="relative flex rounded-full bg-pink-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-pink-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+            <button type="button" onClick={toggleProfileMenu} className="relative flex rounded-full bg-pink-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-pink-800" id="user-menu-button" aria-expanded={showProfileMenu ? 'true' : 'false'} aria-haspopup="true">
               <span className="absolute -inset-1.5"></span>
               <span className="sr-only">Abrir menú</span>
               <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
             </button>
           </div>
 
-       
+          {showProfileMenu && (
           <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
           
             <a href="#" className="block px-4 py-2 text-sm text-pink-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Tu Perfil</a>
             
             <a href="#" onClick={() => actions.logOut()} className="block px-4 py-2 text-sm text-pink-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Cerrar Sesión</a>
           </div>
+           )}
         </div>
       </div>
     </div>
