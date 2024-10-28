f9f255e5600acc3978e21088dead769ee45f1dd5
Diana Jimenez Roo <149723555+DianaJRoo@users.noreply.github.com>
2024-10-28 12:13:50 +0000

    footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,7 +4,7 @@ export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="https://github.com/DianaJRoo">DianaJRoo</a>
+			<a href="https://github.com/DianaJRoo">Diana JRoo</a>
 		</p>
 	</footer>
 );
